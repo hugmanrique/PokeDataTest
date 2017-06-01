50a7ef5ead09ad07abd02129071ad1f6bdd58015
hugmanrique <hugmanrique@gmail.com>
2017-06-01 19:06:52 -0400

    Update 1.0.1

---
+++ src/main/java/me/hugmanrique/pokedatatest/FileLoader.java
@@ -0,0 +1,68 @@
+package me.hugmanrique.pokedatatest;
+
+import lombok.Getter;
+import me.hugmanrique.pokedata.roms.ROMLoader;
+import me.hugmanrique.pokedata.roms.ReadableROM;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.security.CodeSource;
+import java.security.ProtectionDomain;
+
+/**
+ * @author Hugmanrique
+ * @since 28/05/2017
+ */
+public class FileLoader {
+    @Getter
+    private static final File JAR_FOLDER = getJarFolder();
+
+    public static ReadableROM load() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            // Don't exit, we can still use Java's UI
+            e.printStackTrace();
+        }
+
+        FileFilter filter = new FileNameExtensionFilter("GBA ROM", "gba", "bin");
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileFilter(filter);
+        fileChooser.setCurrentDirectory(JAR_FOLDER);
+
+        Action details = fileChooser.getActionMap().get("viewTypeDetails");
+        details.actionPerformed(null);
+
+        if (fileChooser.showOpenDialog(new Frame()) != JFileChooser.APPROVE_OPTION) {
+            return null;
+        }
+
+        File location = fileChooser.getSelectedFile();
+
+        if (location == null) {
+            return null;
+        }
+
+        ReadableROM rom = new ReadableROM();
+
+        try {
+            ROMLoader.load(rom, location);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return rom;
+    }
+
+    private static File getJarFolder() {
+        ProtectionDomain domain = FileLoader.class.getProtectionDomain();
+        CodeSource source = domain.getCodeSource();
+
+        return new File(source.getLocation().getPath());
+    }
+}
--- src/main/java/me/hugmanrique/pokedatatest/PokeDataTest.java
+++ src/main/java/me/hugmanrique/pokedatatest/PokeDataTest.java
@@ -1,13 +1,19 @@
 package me.hugmanrique.pokedatatest;
 
+import me.hugmanrique.pokedata.graphics.ROMImage;
+import me.hugmanrique.pokedata.items.Item;
+import me.hugmanrique.pokedata.loaders.ROMData;
+import me.hugmanrique.pokedata.attacks.Attack;
+import me.hugmanrique.pokedata.pokedex.Pokedex;
+import me.hugmanrique.pokedata.pokedex.PokemonBaseStats;
+import me.hugmanrique.pokedata.pokedex.ev.Evolution;
+import me.hugmanrique.pokedata.pokedex.ev.EvolutionData;
 import me.hugmanrique.pokedata.utils.HeaderNames;
 import me.hugmanrique.pokedata.utils.PokeText;
-import me.hugmanrique.pokedata.utils.ROMLoader;
+import me.hugmanrique.pokedata.roms.ReadableROM;
 
-import javax.swing.*;
-import javax.swing.filechooser.FileFilter;
-import javax.swing.filechooser.FileNameExtensionFilter;
-import java.awt.*;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -19,6 +25,9 @@ public class PokeDataTest {
     public static void main(String[] args) {
         new PokeDataTest(args);
     }
+
+    private ReadableROM rom;
+    private ROMData data;
 
     private PokeDataTest(String[] args) {
         HeaderNames.updateNames();
@@ -33,7 +42,7 @@ public class PokeDataTest {
 
         // TODO implement direct file loading through args
 
-        BaseROM rom = fileLoader();
+        rom = FileLoader.load();
 
         if (rom == null) {
             System.exit(1);
@@ -48,43 +57,90 @@ public class PokeDataTest {
 
         System.out.println(print);
 
-        // Perform tests and data visualization here
-    }
-
-    private BaseROM fileLoader() {
         try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception e) {
-            // Don't exit, we can still use Java's UI
-            e.printStackTrace();
-        }
-
-        FileFilter filter = new FileNameExtensionFilter("GBA ROM", "gba", "bin");
-        JFileChooser fileChooser = new JFileChooser();
-        fileChooser.setFileFilter(filter);
-
-        Action details = fileChooser.getActionMap().get("viewTypeDetails");
-        details.actionPerformed(null);
-
-        if (fileChooser.showOpenDialog(new Frame()) != JFileChooser.APPROVE_OPTION) {
-            return null;
-        }
-
-        File location = fileChooser.getSelectedFile();
-
-        if (location == null) {
-            return null;
-        }
-
-        BaseROM rom = new BaseROM();
-
-        try {
-            ROMLoader.load(rom, location);
+            data = new ROMData(rom);
         } catch (IOException e) {
             e.printStackTrace();
-            return null;
+            System.exit(1);
         }
 
-        return rom;
+        performTests();
+
+        System.exit(0);
     }
+
+    // Perform tests and data visualization here
+    private void performTests() {
+        saveItemImage();
+    }
+
+    private void printPokedex() {
+        for (int i = 0; i < data.getDexEntriesNumber(); i++) {
+            Pokedex pokedex = Pokedex.load(rom, data, i);
+
+            System.out.println(pokedex);
+            System.out.println(pokedex.getDesc1(rom));
+        }
+    }
+
+    private void printItems() {
+        for (int i = 0; i < data.getItemsNumber(); i++) {
+            Item item = Item.load(rom, data, i);
+
+            System.out.println(item);
+            System.out.println(item.getDescription(rom));
+        }
+    }
+
+    private void printMoves() {
+        for (int i = 0; i < data.getAttacksNumber(); i++) {
+            Attack attack = Attack.load(rom, data, i);
+
+            System.out.println(attack);
+        }
+    }
+
+    private void printBaseStats() {
+        for (int i = 0; i < data.getPokemonsNumber(); i++) {
+            PokemonBaseStats stats = PokemonBaseStats.load(rom, data, i);
+
+            System.out.println(stats);
+        }
+    }
+
+    private void printEvolutionData() {
+        for (int i = 0; i < data.getPokemonsNumber(); i++) {
+            EvolutionData evolutionData = EvolutionData.load(rom, data, i);
+            Evolution[] evolutions = evolutionData.getEvolutions();
+
+            for (int k = 0; k < evolutions.length; k++) {
+                System.out.println(evolutions[k]);
+            }
+
+            System.out.println();
+        }
+    }
+
+    private void saveItemImage() {
+        Item item = Item.load(rom, data, 0);
+
+        ROMImage image = item.getImage(rom, data);
+
+        BufferedImage bufferedImage = image.toBufferedImage(true);
+        File file = new File(FileLoader.getJAR_FOLDER(), "item.png");
+
+        try {
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            ImageIO.write(bufferedImage, "png", file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
 }
